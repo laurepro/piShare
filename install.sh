7fe0a823c257c29c3fe4a21ef510bf9c3b6a773f
LAURENT PROTT <50154661+laurepro@users.noreply.github.com>
2020-11-27 19:17:57 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,6 +9,14 @@ chmod -R ug=rwx,o=rx /home/shares/Public
 # install samba
 apt install samba samba-common-bin
 
+# save smb.conf
+FILE=/etc/samba/smb.conf.save
+[ ! -f "$FILE" ] && cp /etc/samba/smb.conf "$FILE"
+# cat public samba
+cat "$FILE" ./install/public.smb > /etc/samba/smb.conf
+# set security to user
+sed '/####### Authentication #######/\   security = user' /etc/samba/smb.conf
+
 # copy udev files
 cd "${0%/*}"
 cp ./install/usb-mount.sh /usr/local/bin/usb-mount.sh
@@ -19,3 +27,4 @@ cp /install/99-local.rules /etc/udev/rules.d/99-local.rules
 # reload rules & services
 udevadm control --reload-rules
 systemctl daemon-reload
+systemctl smbd restart
